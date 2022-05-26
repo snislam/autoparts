b83f824921d788aa4b7a5c8692ce5c2fc22492d1
Shaikh Najmul Islam <najmulbge17@gmail.com>
2022-05-26 22:42:16 +0600

    Manage products done

--- src/Components/Pages/Dashboard/ManageProducts.js
+++ src/Components/Pages/Dashboard/ManageProducts.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../../Shared/Loading';
+import ProductsRow from './ProductsRow';
 
 const ManageProducts = () => {
+    const { data: products, isLoading, refetch } = useQuery('allProductsManage', () => fetch('http://localhost:5000/products', {
+        headers: {
+            'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()))
+
+    if (isLoading) {
+        return <Loading />
+    }
     return (
         <div>
-            <h1>Manage Products</h1>
+            <h1 className='text-3xl font-semibold mb-5'>Manage Products</h1>
+            <table className="table w-full">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Quantity</th>
+                        <th>Minimum Order Quantity</th>
+                        <th>Price</th>
+                        <th>Remove</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        products.map((product, index) => <ProductsRow
+                            key={product._id}
+                            product={product}
+                            index={index}
+                            refetch={refetch}
+                        />)
+                    }
+                </tbody>
+            </table>
         </div>
     );
 };
---
+++ src/Components/Pages/Dashboard/ProductsRow.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { toast } from 'react-toastify';
+
+const ProductsRow = ({ product, index, refetch }) => {
+    const { name, quantity, min_order_qnt, price } = product;
+
+    const handleDelete = () => {
+        // <label for="confirmation-modal" class="btn modal-button">open modal</label>
+        const confirmation = window.confirm('After deleteing it, It remove frome homepage also.');
+
+        if (confirmation) {
+            fetch(`http://localhost:5000/products/${product._id}`, {
+                method: 'DELETE',
+                headers: {
+                    'content-type': 'application/json',
+                    'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+                }
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount) {
+                        toast.warning('Succesfully cancelde.')
+                        refetch();
+                    }
+                })
+        } else {
+            toast("Product kept!")
+        }
+    }
+
+    return (
+        <tr>
+            <th>{index + 1}</th>
+            <td>{name}</td>
+            <td>{quantity} pc.</td>
+            <td>{min_order_qnt} pc.</td>
+            <td>${price}</td>
+            <td><button onClick={handleDelete} className='btn'>Delete</button></td>
+        </tr>
+    );
+};
+
+export default ProductsRow;
